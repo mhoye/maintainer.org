577cda79b7d0e10362add1562ed52e314abce4c5
mhoye <mhoye@bespokeio.com>
2017-02-14 13:26:42 -0500

    Adding envvar for site dir.

--- tools/rebuild-site.sh
+++ tools/rebuild-site.sh
@@ -1,15 +1,13 @@
 #~/bin/sh
 
-# copy the components of the homepage over to index.html
+# Assemble the components of the site; a CMS seems badly overkill at this point.
 # It's written like this on purpose.
+
+SITE=/var/www/maintainer.org
  
 pushd ../../maintainer.org/components/ || exit -100
-for i in `ls *.page | sed "s/.page//"` ; do cat header.common $i.page footer.common > ../site/$i.html ; done
-
-
+for i in `ls *.page | sed "s/.page//"` ; do cat header.common $i.page footer.common > $SITE/$i.html ; done
 
 popd
-
-
 
 exit 0
